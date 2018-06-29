7dcfc94727eaa4bc6a6ff2ce957eef98e4617f39
Julien Lhermitte <lhermitte@bnl.gov>
2018-06-29 11:27:15 -0400

    add probe file sizes

---
+++ probe_file_sizes.py
@@ -0,0 +1,51 @@
+import os
+from datetime import date, timedelta
+import pandas as pd
+import argparse
+
+
+def get_size(start_path = '.'):
+    total_size = 0
+    for dirpath, dirnames, filenames in os.walk(start_path):
+        print(f"checking {dirpath}")
+        for f in filenames:
+            fp = os.path.join(dirpath, f)
+            total_size += os.path.getsize(fp)
+    return total_size
+
+#print get_size()
+
+
+
+def get_filesize_table(root_dir):
+    # start and end this date
+    d1 = date(2015, 1, 1)  # start date
+    d2 = date(2018, 7, 1)  # end date
+    #ROOT = "/home/lhermitte/tmp"
+
+    delta = d2 - d1         # timedelta
+
+    df = pd.DataFrame()
+
+    for i in range(delta.days + 1):
+        new_date = d1 + timedelta(i)
+        subdir = f'{new_date.year:04}/{new_date.month:02}/{new_date.day:02}'
+        sz = get_size(root_dir + "/" + subdir)
+        df = df.append(pd.Series(dict(date=new_date, size=sz)),
+                       ignore_index=True)#, name=new_date))
+    return df
+
+if __name__ == "__main__":
+    '''
+        Walks through directories in a yyyy/mm/dd structure and obtains file
+        sizes. Saves file size total per day.
+    '''
+    parser = argparse.ArgumentParser(description='Scrape the date directory tree for file sizes.')
+    parser.add_argument('-r', '--root', dest='root_dir', type=str,
+                    help='The root directory', default='.')
+    parser.add_argument('-o', '--output_file', dest='output_file', type=str,
+                    help='The root directory', default='dat.txt')
+
+    args = parser.parse_args()
+    df = get_filesize_table(args.root_dir)
+    df.to_csv(args.output_file, sep=" ", index=False)
